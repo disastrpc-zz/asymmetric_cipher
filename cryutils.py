1c1230e4089307c5565b5f07f349da9a8ea1b754
0ddguy <jaredfreed@gmail.com>
2019-08-30 09:58:09 -0400

    Fixed cryutils modInverse ValueError. Public and private key generation are handled by its own class _KeyGenerator and key containment and formatting is handled by KeyContainer class

--- cryutils.py
+++ cryutils.py
@@ -82,9 +82,9 @@ def trialDiv(n):
 def modInverse(a, m):
     if math.gcd(a, m) != 1:
         return None
-    u1,u2,u3 = 1,0,a
-    v1,v2,v3 = 0,1,m
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
     while v3 != 0:
         q = u3 // v3
-        v1,v2,v3,u1,u2,u3 = (u1 - q * v2),(u3 - q * v3), v1,v2,v3
+        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1 ,v2, v3
     return u1 % m
--- keygen.py
+++ keygen.py
@@ -6,17 +6,13 @@
 # d = e mod inv 
 import cryutils, math, random
 
-class _ComputeKey:
+class _KeyGenerator:
 
     def __init__(
             self,
-            public_key=0, 
-            private_key=0, 
             keysize=1024,
             n=0,e=0,d=0,p=0,q=0):
 
-        self.public_key = public_key
-        self.private_key = private_key
         self.keysize = keysize
         self.n = n
         self.e = e
@@ -24,33 +20,56 @@ class _ComputeKey:
         self.p = cryutils.genPrime(self.keysize)
         self.q = cryutils.genPrime(self.keysize)
     
-    def _comp_n(self, n, p, q):
+    def _comp_n(self, p, q):
         self.n = self.p * self.q
         return self.n
     
-    def _comp_e(self, keysize):
+    def _comp_e(self, p, q):
         self.x = (self.p - 1) * (self.q - 1)
-        print(" " + str(self.x) + " ")
         if (self.p > self.q): 
             while True:      
-                self.e = random.randrange(self.q, self.p)
+                self.e = random.randrange(self.q, self.p) # ?
                 if(math.gcd(self.e,self.x)==1):
                     break
             return self.e
         else:
             while True:
-                self.e = random.randrange(self.p, self.q)
+                self.e = random.randrange(self.p, self.q) # ?
                 if(math.gcd(self.e,self.x)==1):
                     break
             return self.e
     
-    def _comp_d(self, d, e, p, q):
+    def _comp_d(self, e, p, q):
         self.d = cryutils.modInverse(self.e, (self.p - 1) * (self.q - 1))
         return self.d
 
-class KeyContainer(_ComputeKey):
+class KeyContainer(_KeyGenerator):
     
-    def generate():
-        k = _ComputeKey()
+    def __init__(self, private_key=0, public_key=0):
+        self.private_key = private_key
+        self.public_key = public_key
+    
+    def generate(self):
+        self.gen = _KeyGenerator()
+        self.n = self.gen._comp_n(self.gen.p, self.gen.q)
+        self.e = self.gen._comp_e(self.gen.p, self.gen.q)
+        self.d = self.gen._comp_d(self.e, self.gen.p, self.gen.q)
+        return self.n, self.e, self.d
+
+def main():
+    keygen = _KeyGenerator()
+    n = keygen._comp_n(keygen.p, keygen.q)
+    print(n)
+    e = keygen._comp_e(keygen.p, keygen.q)
+    print(e)
+    d = keygen._comp_d(e, keygen.p, keygen.q)
+    print(d)
+
+if __name__ == "__main__":
+    main()
+        
+
+        
+
         
 
